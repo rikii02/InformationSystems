1a2e15d1d76fe608c42f60ac02d71978b37e5f5f
rikii02 <rikigodia@gmail.com>
2024-04-28 19:40:38 +0200

    accounting page

--- frontend/src/pages/AccountingPage.js
+++ frontend/src/pages/AccountingPage.js
@@ -1,11 +1,18 @@
-// OwnersPage.js
 import React from 'react';
-
+import '../styles/AccountingPage.css'
 function AccountingPage() {
+
+    const handleUploadButtonClick = () => {
+        // Aquí puedes implementar la lógica para abrir la ventana emergente o modal para cargar un archivo
+        // Por ejemplo, puedes mostrar un modal usando un estado para controlar su visibilidad
+    };
+
     return (
         <div className="accounting">
-            <h1>Accounting Page</h1>
-            <p>This is the Accounting page content.</p>
+            <div className='accounting-header'>
+                <h1 className='accounting-title'>Accounting</h1>
+                <button className="upload-button" onClick={handleUploadButtonClick}>Upload File</button>
+            </div>
         </div>
     );
 }
