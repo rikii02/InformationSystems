7dda6963f8d19893937c75b0fc126976584b8f3f
rikii02 <rikigodia@gmail.com>
2024-05-12 22:01:34 +0200

    update

--- frontend/src/pages/AccountingPage.js
+++ frontend/src/pages/AccountingPage.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import '../styles/AccountingPage.css'
+// accounting-page.js
+import React, { useState } from 'react';
+import '../styles/AccountingPage.css';
+import '../styles/AccountingModal.css';
+
 function AccountingPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleUploadButtonClick = () => {
-        // Aquí puedes implementar la lógica para abrir la ventana emergente o modal para cargar un archivo
-        // Por ejemplo, puedes mostrar un modal usando un estado para controlar su visibilidad
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log('Selected file:', file);
+        }
+        handleCloseModal();
     };
 
     return (
@@ -13,6 +28,21 @@ function AccountingPage() {
                 <h1 className='accounting-title'>Accounting</h1>
                 <button className="upload-button" onClick={handleUploadButtonClick}>Upload File</button>
             </div>
+            {isModalOpen && (
+                <div className="modal-container" onClick={handleCloseModal}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <h2>Upload Accounting</h2>
+                        <p>Select the accounting data. Make sure the file is in CSV format.</p>
+                        <input
+                            type="file"
+                            onChange={handleFileUpload}
+                            accept=".csv"
+                            lang="en"
+                        />
+                        <button onClick={handleCloseModal}>Cancel</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- frontend/src/pages/PropertiesPage.js
+++ frontend/src/pages/PropertiesPage.js
@@ -1,20 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import propertiesData from '../dbcontent/properties.json'; 
-import '../styles/PropertiesPage.css'
+import '../styles/PropertiesPage.css';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'; // Importar los iconos de flecha
+
 function PropertiesPage() {
-    const [properties, setProperties] = useState([]); 
+    const [expandedGroups, setExpandedGroups] = useState([]);
 
-    useEffect(() => {
-        setProperties(propertiesData); 
-    }, []);
+    const toggleGroupExpansion = (group) => {
+        if (expandedGroups.includes(group)) {
+            setExpandedGroups(expandedGroups.filter((expandedGroup) => expandedGroup !== group));
+        } else {
+            setExpandedGroups([...expandedGroups, group]);
+        }
+    };
 
-   
+    const buildingProperties = propertiesData.reduce((acc, property) => {
+        if (!acc[property.building]) {
+            acc[property.building] = [];
+        }
+        acc[property.building].push(property);
+        return acc;
+    }, {});
+
     return (
         <div className="properties">
             <h1>Properties</h1>
             <table>
                 <thead>
-                    <tr>  
+                    <tr>
+                        <th>Building</th>
                         <th>Property ID</th>
                         <th>Property Status</th>
                         <th>Property Type</th>
@@ -27,18 +41,33 @@ function PropertiesPage() {
                     </tr>
                 </thead>
                 <tbody>
-                    {properties.map(property => (
-                        <tr key={property.id}>
-                            <td>{property.id}</td>
-                            <td>{property.status}</td>
-                            <td>{property.type}</td>
-                            <td>{property.location}</td>
-                            <td>{property.rentalprice}</td>
-                            <td>{property.clientid}</td>
-                            <td>{property.clientname}</td>
-                            <td>{property.leaseid}</td>
-                            <td>{property.leaseexpdate}</td>
-                        </tr>
+                    {Object.keys(buildingProperties).map((building) => (
+                        <React.Fragment key={building}>
+                            
+                            <tr className="group-row" onClick={() => toggleGroupExpansion(building)}>
+                                <td>
+                                    <span>{building}</span>
+                                    <span className="arrow-icon">
+                                        {expandedGroups.includes(building) ? <FaChevronUp /> : <FaChevronDown />}
+                                    </span>
+                                </td>
+                                <td colSpan="9"></td>
+                            </tr>
+                            {expandedGroups.includes(building) && buildingProperties[building].map((property, index) => (
+                                <tr key={property.id}>
+                                    {index === 0 ? <td rowSpan={buildingProperties[building].length}>{building}</td> : null}
+                                    <td>{property.id}</td>
+                                    <td>{property.status}</td>
+                                    <td>{property.type}</td>
+                                    <td>{property.location}</td>
+                                    <td>{property.rentalprice}</td>
+                                    <td>{property.clientid}</td>
+                                    <td>{property.clientname}</td>
+                                    <td>{property.leaseid}</td>
+                                    <td>{property.leaseexpdate}</td>
+                                </tr>
+                            ))}
+                        </React.Fragment>
                     ))}
                 </tbody>
             </table>
